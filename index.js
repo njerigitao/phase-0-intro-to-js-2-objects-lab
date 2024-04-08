228d538e3524c11a7dfda7e1ca814a9f4474e3e3
AGNESGITAU <agnesgitau40@gmail.com>
2024-04-08 07:23:53 +0300

    intro-to-js-objects

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+const employee = {
+    name: "Max",
+    streetAddress: "5 Main Street",
+};
+function updateEmployeeWithKeyAndValue(employee,Key, value){
+    return Object.assign({}, employee, {[Key]: value});
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, Key, value){
+    employee[Key] = value
+    return employee;
+}
+function deleteFromEmployeeByKey(employee,Key){
+    const newObj = Object.assign({}, employee);
+    delete newObj[Key];
+    return newObj;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, Key){
+    delete employee[Key];
+    return employee;
+}
